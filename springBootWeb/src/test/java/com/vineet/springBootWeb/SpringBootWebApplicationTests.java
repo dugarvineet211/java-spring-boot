6f73465d1239764ed6b3f838b7b9d715084bd305
Vineet Dugar <dugarvineet211@gmail.com>
2024-07-10 22:07:38 +0530

    project structure setup

---
+++ springBootWeb/src/test/java/com/vineet/springBootWeb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineet.springBootWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
